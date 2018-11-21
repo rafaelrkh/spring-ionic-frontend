a4db9462a695d7b91becba6f091c78c322815392
Rafael <rafael__ptc@hotmail.com>
2018-11-21 20:06:08 -0200

    Salvando os dados do usuário logado no LocalStorage

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -33,7 +33,7 @@ export class HomePage {
   public login(){
     this.auth.authenticate(this.credencial)
              .subscribe(response => {
-               console.log(response.headers.get('Authorization'));
+               this.auth.successfulLogin(response.headers.get('Authorization'));
                this.navCtrl.setRoot('CategoriasPage');
              },
              error => {});
