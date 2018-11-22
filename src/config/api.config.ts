2aceaf39e8ee90c05e0d4ccb195faac48364397d
Rafael <rafael__ptc@hotmail.com>
2018-11-22 21:39:23 -0200

    Carregando imagens dos produtos

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,5 +1,5 @@
 export const API_CONFIG = {
     baseUrl: "http://localhost:9080",
-    bucketBaseUrl: "https://s3-sa-east-1.amazonaws.com/curso.spring.ionic"
+    bucketBaseUrl: "https://s3-sa-east-1.amazonaws.com/spring-ionic-puc"
     
 }
