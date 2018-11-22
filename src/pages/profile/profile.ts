4afbf3f9e3adb3b039689e3c0396deff2ff73442
Rafael <rafael__ptc@hotmail.com>
2018-11-22 10:15:40 -0200

    Tratando erros 403

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -15,10 +15,11 @@ export class ProfilePage {
 
   cliente: ClienteDTO;
 
-  constructor(public navCtrl: NavController
-    , public navParams: NavParams
-    , public storage: StorageService
-    , public clienteService : ClienteService) {
+  constructor(
+    public navCtrl: NavController, 
+    public navParams: NavParams,
+    public storage: StorageService,
+    public clienteService: ClienteService) {
   }
 
   ionViewDidLoad() {
@@ -29,15 +30,22 @@ export class ProfilePage {
           this.cliente = response;
           this.getImageIfExists();
         },
-        error => {});
+        error => {
+          if (error.status == 403) {
+            this.navCtrl.setRoot('HomePage');
+          }
+        });
+    }
+    else {
+      this.navCtrl.setRoot('HomePage');
     }
   }
-   getImageIfExists() {
+
+  getImageIfExists() {
     this.clienteService.getImageFromBucket(this.cliente.cdCliente)
     .subscribe(response => {
       this.cliente.imageUrl = `${API_CONFIG.bucketBaseUrl}/cp${this.cliente.cdCliente}.jpg`;
     },
     error => {});
   }
-
 }
