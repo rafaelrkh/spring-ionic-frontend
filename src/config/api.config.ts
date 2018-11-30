e3feb9435856ff22dd3abfab9d09627aa83a0945
Rafael Henrique <rafael__ptc@hotmail.com>
2018-11-30 00:08:34 -0200

    Ultimas alterações pt2

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,6 +1,6 @@
 export const API_CONFIG = {
-   // baseUrl: "https://spring-heroku-ionic.herokuapp.com/",
-    baseUrl: "http://localhost:9080/",
+    baseUrl: "https://spring-heroku-ionic.herokuapp.com/",
+   // baseUrl: "http://localhost:9080/",
     bucketBaseUrl: "https://s3-sa-east-1.amazonaws.com/spring-ionic-puc"
     
 }
